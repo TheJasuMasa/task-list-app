b790fa5f179d7dd56ec9d4cd88c31f1b117f47be
Justin Smith <justin.smith@achievegoal.jp>
2021-12-06 14:04:40 +0900

    create and begin to configure AddTask component functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 import { useState } from 'react'
 
 function App() {
@@ -27,14 +28,27 @@ function App() {
 
  //Delete Task
  const deleteTask = (id) => {
-   console.log('delete', id)
    setTasks(tasks.filter((task)=> task.id !== id))
+ }
+
+//Toggle Reminder
+ const toggleReminder = (id) => {
+   setTasks(
+     tasks.map((task)=>
+     task.id === id ? {...task, reminder: !task.reminder} :
+    task
+    )
+   )
  }
 
   return (
     <div className="container">
       <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : <p>No Tasks  to Show</p>}
+      <AddTask />
+      {tasks.length > 0 ? <Tasks
+      onToggle = {toggleReminder}
+      tasks={tasks} 
+      onDelete={deleteTask}/> : <p>No Tasks  to Show</p>}
     </div>
   );
 }
